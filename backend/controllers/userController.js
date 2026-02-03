ed3eff99c0109b49ce09b8b2c6f6e1e0cffc238f
Chukwu Felix <goduknows@gmail.com>
2026-02-03 15:28:18 +0100

    return statement for getting user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -157,7 +157,7 @@ const getUserProfile = async (req, res) => {
             const user = await User.findById(userId).select("-password");
 
             if (!user) {
-                  res.status(404).json({ message: "User not found" });
+                  return res.status(404).json({ message: "User not found" });
             }
 
             res.status(200).json(user);
